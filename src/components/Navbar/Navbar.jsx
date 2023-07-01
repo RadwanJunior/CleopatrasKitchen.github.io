d55fa832f55aa5bcbb5745e1246bdd2513bc0c50
Adham Radwan <aradw009@uottawa.ca>
2023-07-01 14:39:32 -0400

    latest commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,43 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
-import {MdOutlineRestaurantMenu} from 'react-icons/md';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
 
 import images from '../../constants/images';
-
 import './Navbar.css';
 
 const Navbar = () => (
-  <nav class = "app_navbar">
-    <div class = "app_navbar-logo">
-      <img src={images.gericht} alt="app logo"/>
+  <nav className="app__navbar">
+    <div className="app__navbar-logo">
+      <img src={images.gericht} alt="logo" />
     </div>
     <ul className="app__navbar-links">
-      <li className="p__opensans"><a href="#home">Home</a></li> 
+      <li className="p__opensans"><a href="#home">Home</a></li>
       <li className="p__opensans"><a href="#about">About</a></li>
       <li className="p__opensans"><a href="#menu">Menu</a></li>
       <li className="p__opensans"><a href="#awards">Awards</a></li>
-      <li className="p__opensans"><a href="#contact">Contact</a></li> 
+      <li className="p__opensans"><a href="#contact">Contact</a></li>
     </ul>
     <div className="app__navbar-login">
-      <a href="#login" className='p__opensans'>Log In / Register</a>
+      <a href="#login" className="p__opensans">Log In / Register</a>
+      <div />
+      <a href="/" className="p__opensans">Book Table</a>
     </div>
-    <a href="/" className="p__opensans">Book Table</a>
-    <div className="app__navbar-smallscreen">
-      <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => {}} />
+    <div className="app__navbar-smallscreen-links">
+      <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => { }} />
+      
+      <div className="app__navbar-smallscreen_overlay flex_center slide-bottom">
 
-    <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
-        <MdOutlineRestaurantMenu fontSize={27} className="overlay_close" onClick={() => {}} />
-      <ul className="app__navbar-smallscreen-links">
-        <li className="p__opensans"><a href="#home">Home</a></li> 
-        <li className="p__opensans"><a href="#about">About</a></li>
-        <li className="p__opensans"><a href="#menu">Menu</a></li>
-        <li className="p__opensans"><a href="#awards">Awards</a></li>
-        <li className="p__opensans"><a href="#contact">Contact</a></li> 
-      </ul>
+      </div>
+      
+      <li className="p__opensans"><a href="#home">Home</a></li>
+      <li className="p__opensans"><a href="#about">About</a></li>
+      <li className="p__opensans"><a href="#menu">Menu</a></li>
+      <li className="p__opensans"><a href="#awards">Awards</a></li>
+      <li className="p__opensans"><a href="#contact">Contact</a></li>
     </div>
-    </div>
+
   </nav>
-
 );
 
 export default Navbar;
