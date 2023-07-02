12a083393c5078562a15d61f30c17333f2593285
Adham Radwan <aradw009@uottawa.ca>
2023-07-02 14:59:41 -0400

    completed header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
+import { images } from '../../constants';
 import './Header.css';
+import { SubHeading } from '../../components';
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavour"/>
+      <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: '2rem 0' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec quam sed tortor lacinia ultricies. Morbi varius lectus nec quam lobortis, ac dignissim nulla consectetur. Proin in metus at ante rhoncus facilisis sed et nunc. Sed ultrices nunc a tristique sollicitudin. </p>
+      <button type="button" className="custom__button">Explore Menu</button>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header img"/>
+    </div>
   </div>
 );
 
