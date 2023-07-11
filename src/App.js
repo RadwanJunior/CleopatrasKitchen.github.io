c0b88a7ed547389ffa57b235b34833d10b0680a1
Adham Radwan <aradw009@uottawa.ca>
2023-07-11 14:00:56 -0400

    Implemented React Router to display multiple pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import AboutUs from './pages/AboutUs';
+import Contact from './pages/Contact';
+import History from './pages/History';
+import Menu from './pages/Menu';
+import Photos from './pages/Photos';
+import NotFound from './pages/NotFound';
 
-import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
-import { Navbar } from './components';
 import './App.css';
 
 const App = () => (
   <div>
-    <Navbar />
-    <Header />
-    <AboutUs />
-    <SpecialMenu />
-    <Chef />
-    <Intro />
-    <Laurels />
-    <Gallery />
-    <FindUs />
-    <Footer />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/history" element={<History />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/photos" element={<Photos />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   </div>
 );
 
