d059c19e349f0b461732c6a275e5933ccea139ab
Adham Radwan <aradw009@uottawa.ca>
2023-07-13 16:30:23 -0400

    Updated cards to add text before hovering

--- src/container/AboutUsMission/MissionValues.jsx
+++ src/container/AboutUsMission/MissionValues.jsx
@@ -12,6 +12,7 @@ const MissionValues = () => (
                             <h3>Commitment to Excellence</h3>
                             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. At amet incidunt magnam fugit, maiores doloribus ut obcaecati, quibusdam provident voluptates neque quo? Deleniti laudantium commodi harum autem totam doloribus mollitia.</p>
                         </div>
+                        <div className='text'>Commitment to Excellence</div>
                     </div>
                 <div class="box">
                     <div class="icon"><i class="fa fa-circle" aria-hidden="true"></i></div>
@@ -19,6 +20,7 @@ const MissionValues = () => (
                         <h3>Customer Satisfaction</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem magnam odio ratione ullam ex eaque labore porro aperiam vero! Voluptatem facilis enim dolor consequuntur exercitationem laborum aliquid mollitia reiciendis.</p>
                     </div>
+                    <div className='text'>Customer Satisfaction</div>
                 </div>
                 <div class="box">
                     <div class="icon"><i class="fa fa-circle" aria-hidden="true"></i></div>
@@ -26,6 +28,7 @@ const MissionValues = () => (
                         <h3>Integrity and Transparency</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam unde sed enim veritatis quod exercitationem voluptates et facere fuga odio, blanditiis vel debitis atque repudiandae fugiat quis minus, maxime deleniti?</p>
                     </div>
+                    <div className='text'>Integrity and Transparency</div>
                 </div>
             </div>
     </section>
