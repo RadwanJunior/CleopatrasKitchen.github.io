0974efd514feb8c58ab6ec3a0c6a4aa95276065d
Adham Radwan <aradw009@uottawa.ca>
2023-07-14 15:47:43 -0400

    Created timeline in our history

--- src/container/Timeline/Timeline.jsx
+++ src/container/Timeline/Timeline.jsx
@@ -1,1 +1,21 @@
-import "./Timeline.css"
+import React from 'react';
+import events from './events.json';
+import './Timeline.css';
+
+const Timeline = () => (
+    <div className="timeline-wrapper">
+        <div className="timeline-container">
+            {events.map((event, idx) => (
+                <div key={event.id} className={`timeline-item timeline-item-${idx % 2 === 0 ? 'left' : 'right'}`}>
+                    <div className="timeline-content">
+                        <h2>{event.date}</h2>
+                        <p>{event.event}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    </div>
+
+);
+
+export default Timeline;
--- src/pages/History.js
+++ src/pages/History.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import AlternateNavbar from '../components/AlternateNavbar/AlternateNavbar';
+import Timeline from '../container/Timeline/Timeline';
 import Footer from '../container/Footer/Footer';
 
 export default function () {
     return (
         <div>
             <AlternateNavbar />
-            <h1>History</h1>
+            <Timeline />
             <Footer />
         </div>
     );
