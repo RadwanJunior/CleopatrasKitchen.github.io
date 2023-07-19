9f9f7b055722ed3788e0fea7bb646d18bcfdc0dc
Adham Radwan <aradw009@uottawa.ca>
2023-07-18 20:41:06 -0400

    reservation

--- src/container/Reservation/Reservation.jsx
+++ src/container/Reservation/Reservation.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
-import { Container, Typography, TextField, Button, Box } from '@mui/material';
+import { Stepper, Step, StepLabel, Typography, Button, TextField, Container } from '@mui/material';
+
+const steps = ['Contact Information', 'Reservation Details', 'Special Requests'];
 
 const Reservation = () => {
-  const [reservation, setReservation] = useState({
+  const [activeStep, setActiveStep] = useState(0);
+  const [formData, setFormData] = useState({
     name: '',
     email: '',
     phone: '',
@@ -12,47 +15,78 @@ const Reservation = () => {
     requests: '',
   });
 
-  const handleInputChange = (event) => {
-    setReservation({ ...reservation, [event.target.name]: event.target.value });
+  const isStepSkipped = (step) => {
+    return formData[step] === '';
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    localStorage.setItem('reservation', JSON.stringify(reservation));
-    alert('Reservation submitted');
-    setReservation({
-      name: '',
-      email: '',
-      phone: '',
-      date: '',
-      time: '',
-      people: '',
-      requests: '',
-    });
+  const handleNext = () => {
+    let newSkipped = isStepSkipped(steps[activeStep]);
+
+    if (!newSkipped) {
+      setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    } else {
+      alert('Please fill in all fields before proceeding');
+    }
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  const handleInputChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
   };
 
   return (
     <Container>
       <Typography variant="h2" sx={{ color: '#DCCA87', fontFamily: 'Cormorant Upright, serif', mb: 3 }}>Make a Reservation</Typography>
-      <Box
-        component="form"
-        onSubmit={handleSubmit}
-        sx={{
-          '& .MuiTextField-root': { m: 1, width: '25ch' },
-          '& .MuiButton-root': { m: 2 },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField label="Name" variant="outlined" name="name" value={reservation.name} onChange={handleInputChange} />
-        <TextField label="Email" variant="outlined" name="email" value={reservation.email} onChange={handleInputChange} />
-        <TextField label="Phone Number" variant="outlined" name="phone" value={reservation.phone} onChange={handleInputChange} />
-        <TextField label="Date (YYYY-MM-DD)" variant="outlined" name="date" value={reservation.date} onChange={handleInputChange} />
-        <TextField label="Time" variant="outlined" name="time" value={reservation.time} onChange={handleInputChange} />
-        <TextField label="Number of People" variant="outlined" name="people" value={reservation.people} onChange={handleInputChange} />
-        <TextField label="Special Requests" variant="outlined" name="requests" value={reservation.requests} onChange={handleInputChange} />
-        <Button variant="contained" color="primary" type="submit">Reserve</Button>
-      </Box>
+      <Stepper activeStep={activeStep} sx={{ mb: 3 }}>
+        {steps.map((label) => {
+          return (
+            <Step key={label}>
+              <StepLabel>{label}</StepLabel>
+            </Step>
+          );
+        })}
+      </Stepper>
+
+      <div>
+        {activeStep === steps.length ? (
+          <div>
+            <Typography sx={{ mb: 2 }}>All steps completed</Typography>
+            <Button onClick={handleReset}>Reset</Button>
+          </div>
+        ) : (
+          <div>
+            {activeStep === 0 && (
+              <div>
+                <TextField label="Name" variant="outlined" name="name" value={formData.name} onChange={handleInputChange} sx={{ mb: 2, mr: 2 }} />
+                <TextField label="Email" variant="outlined" name="email" value={formData.email} onChange={handleInputChange} sx={{ mb: 2, mr: 2 }} />
+                <TextField label="Phone Number" variant="outlined" name="phone" value={formData.phone} onChange={handleInputChange} sx={{ mb: 2 }} />
+              </div>
+            )}
+            {activeStep === 1 && (
+              <div>
+                <TextField label="Date (YYYY-MM-DD)" variant="outlined" name="date" value={formData.date} onChange={handleInputChange} sx={{ mb: 2, mr: 2 }} />
+                <TextField label="Time" variant="outlined" name="time" value={formData.time} onChange={handleInputChange} sx={{ mb: 2, mr: 2 }} />
+                <TextField label="Number of People" variant="outlined" name="people" value={formData.people} onChange={handleInputChange} sx={{ mb: 2 }} />
+              </div>
+            )}
+            {activeStep === 2 && (
+              <TextField label="Special Requests" variant="outlined" name="requests" value={formData.requests} onChange={handleInputChange} sx={{ mb: 2 }} />
+            )}
+            
+            <div>
+              <Button disabled={activeStep === 0} onClick={handleBack} sx={{ mr: 1 }}>Back</Button>
+              <Button variant="contained" color="primary" onClick={handleNext}>{activeStep === steps.length - 1 ? 'Finish' : 'Next'}</Button>
+            </div>
+          </div>
+        )}
+      </div>
     </Container>
   );
 };
