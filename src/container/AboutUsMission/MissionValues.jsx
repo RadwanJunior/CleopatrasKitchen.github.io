b9e70435779b1c3806a4047eb3ca1dfc27be6ca9
Adham Radwan <aradw009@uottawa.ca>
2023-07-21 12:07:50 -0400

    Filled placeholder text

--- src/container/AboutUsMission/MissionValues.jsx
+++ src/container/AboutUsMission/MissionValues.jsx
@@ -4,33 +4,33 @@ import './MissionValues.css';
 
 const MissionValues = () => (
     <section className="mission-values">
-            <h2 className="mission-values-h2">Our Mission and Values</h2>
-            <div class="container">
-                    <div class="box">
-                        <div class="icon"><i class="fa fa-circle" aria-hidden="true"></i></div>
-                        <div class="content">
-                            <h3>Commitment to Excellence</h3>
-                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. At amet incidunt magnam fugit, maiores doloribus ut obcaecati, quibusdam provident voluptates neque quo? Deleniti laudantium commodi harum autem totam doloribus mollitia.</p>
-                        </div>
-                        <div className='text'>Commitment to Excellence</div>
-                    </div>
-                <div class="box">
-                    <div class="icon"><i class="fa fa-circle" aria-hidden="true"></i></div>
-                    <div class="content">
-                        <h3>Customer Satisfaction</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem magnam odio ratione ullam ex eaque labore porro aperiam vero! Voluptatem facilis enim dolor consequuntur exercitationem laborum aliquid mollitia reiciendis.</p>
-                    </div>
-                    <div className='text'>Customer Satisfaction</div>
+        <h2 className="mission-values-h2">Our Mission and Values</h2>
+        <div className="container">
+            <div className="box">
+                <div className="icon"><i className="fa fa-circle" aria-hidden="true"></i></div>
+                <div className="content">
+                    <h3>Exquisite Seafood</h3>
+                    <p>We're dedicated to crafting exquisite seafood dishes that showcase the flavors of the Mediterranean and Red Sea.</p>
                 </div>
-                <div class="box">
-                    <div class="icon"><i class="fa fa-circle" aria-hidden="true"></i></div>
-                    <div class="content">
-                        <h3>Integrity and Transparency</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam unde sed enim veritatis quod exercitationem voluptates et facere fuga odio, blanditiis vel debitis atque repudiandae fugiat quis minus, maxime deleniti?</p>
-                    </div>
-                    <div className='text'>Integrity and Transparency</div>
-                </div>
+                <div className="text">Exquisite Seafood</div>
             </div>
+            <div className="box">
+                <div className="icon"><i className="fa fa-circle" aria-hidden="true"></i></div>
+                <div className="content">
+                    <h3>Unparalleled Service</h3>
+                    <p>Our mission is to provide unparalleled customer service, ensuring every visit is exceptional.</p>
+                </div>
+                <div className="text">Unparalleled Service</div>
+            </div>
+            <div className="box">
+                <div className="icon"><i className="fa fa-circle" aria-hidden="true"></i></div>
+                <div className="content">
+                    <h3>Integrity & Transparency</h3>
+                    <p>We conduct our business with integrity, transparency, and a commitment to quality and hygiene.</p>
+                </div>
+                <div className="text">Integrity & Transparency</div>
+            </div>
+        </div>
     </section>
 );
 
